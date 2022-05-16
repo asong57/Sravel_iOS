a8fd8f263ed56b647faee465994668be2ad5cfd5
asong57 <1998thddk@gmail.com>
2022-05-16 22:48:59 +0900

    [Feat] CustomColor Constant 추가

---
+++ Sravel/Sravel/Presentation/Login/View/LoginViewController.swift
@@ -0,0 +1,8 @@
+//
+//  LoginViewController.swift
+//  Sravel
+//
+//  Created by asong on 2022/05/16.
+//
+
+import Foundation
---
+++ Sravel/Sravel/Util/UIColor+CustomColor.swift
@@ -0,0 +1,30 @@
+//
+//  UIColor+CustomColor.swift
+//  Sravel
+//
+//  Created by asong on 2022/05/16.
+//
+
+import UIKit
+
+extension UIColor {
+    static var darkBlue: UIColor {
+        return UIColor(red: 38.0 / 255.0, green: 153.0 / 255.0, blue: 251.0 / 255.0, alpha: 1)
+    }
+
+    static var blue: UIColor {
+        return UIColor(red: 127.0 / 255.0, green: 196.0 / 255.0, blue: 253.0 / 255.0, alpha: 1)
+    }
+    
+    static var midBlue: UIColor {
+        return UIColor(red: 188.0 / 255.0, green: 224.0 / 255.0, blue: 253.0 / 255.0, alpha: 1)
+    }
+    
+    static var lightBlue: UIColor {
+        return UIColor(red: 241.0 / 255.0, green: 249.0 / 255.0, blue: 255.0 / 255.0, alpha: 1)
+    }
+    
+    static var white: UIColor {
+        return UIColor(red: 255.0 / 255.0, green: 255.0 / 255.0, blue: 255.0 / 255.0, alpha: 1)
+    }
+}

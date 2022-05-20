2ef1e1ffd935cc24f39920b91c8cc8fb4e597407
asong57 <1998thddk@gmail.com>
2022-05-20 10:56:53 +0900

    [Feat] Firebase pod 추가

--- Sravel/Sravel/Presentation/Login/ViewModel/LoginViewModel.swift
+++ Sravel/Sravel/Presentation/Login/ViewModel/LoginViewModel.swift
@@ -6,6 +6,27 @@
 //
 
 import Foundation
+import RxSwift
+import RxRelay
 
 class LoginViewModel {
+    private let disposeBag = DisposeBag()
+    var input = Input()
+    var output = Output()
+    
+    struct Input {
+        let email = PublishSubject<String>()
+        let password = PublishSubject<String>()
+        let tapSignIn = PublishSubject<Void>()
+    }
+    
+    struct Output {
+        let enableSignInButton = PublishRelay<Bool>()
+        let errorMessage = PublishRelay<String>()
+        let goToMain = PublishRelay<Void>()
+    }
+    
+    init(){
+        
+    }
 }

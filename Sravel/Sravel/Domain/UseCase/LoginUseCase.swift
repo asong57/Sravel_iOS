adab7ced45a9c71f99d932ce26f79b39d32349af
asong57 <1998thddk@gmail.com>
2022-05-23 22:27:02 +0900

    [Feat] LoginUseCase 구성

--- Sravel/Sravel/Domain/UseCase/LoginUseCase.swift
+++ Sravel/Sravel/Domain/UseCase/LoginUseCase.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+import RxSwift
+
+class LoginUseCase{
+    var email: String = ""
+    var password: String = ""
+    var emailValidationState = BehaviorSubject<Bool>(value: false)
+    
+    func validate(text: String) {
+        self.email = text
+        self.updateValidationState(of: text)
+    }
+    
+    private func updateValidationState(of emailText: String) {
+        guard !emailText.isEmpty else {
+            self.emailValidationState.onNext(false)
+            return
+        }
+        guard emailText.count >= 5 else {
+            self.emailValidationState.onNext(false)
+            return
+        }
+        guard emailText.count <= 20 else {
+            self.emailValidationState.onNext(false)
+            return
+        }
+        guard emailText.range(of: "[^a-zA-Z0-9]", options: .regularExpression) == nil else {
+            self.emailValidationState.onNext(false)
+            return
+        }
+        
+        self.emailValidationState.onNext(true)
+    }
+}

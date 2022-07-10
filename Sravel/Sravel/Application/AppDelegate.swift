0c63aaedd07a64f4f5b22c5604dfe79c779cd117
asong57 <1998thddk@gmail.com>
2022-07-10 21:56:34 +0900

    [Feat] api key hide

--- Sravel/Sravel/Application/AppDelegate.swift
+++ Sravel/Sravel/Application/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
-        GMSServices.provideAPIKey("")
+        GMSServices.provideAPIKey(Storage().apiKey)
         return true
     }
 
---
+++ Sravel/Sravel/Storage.swift
@@ -0,0 +1,8 @@
+//
+//  Storage.swift
+//  Sravel
+//
+//  Created by asong on 2022/07/10.
+//
+
+import Foundation

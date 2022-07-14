04e117c85a9d762b9710ebabb72158fd9cfde162
asong57 <1998thddk@gmail.com>
2022-07-14 22:42:31 +0900

    [Feat] MarkerData SnapShotDTO로 파싱

--- Sravel/Sravel/Data/Database/Firestore.swift
+++ Sravel/Sravel/Data/Database/Firestore.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+import FirebaseCore
+import FirebaseFirestore
+import RxSwift
+
+class FireStore{
+    static let fireStore = FireStore()
+    private init(){}
+    
+    private let db = Firestore.firestore()
+    
+    func getMarkersLocationSet() -> Observable<[SnapShotDTO]> {
+        print("Get Markers 실행됨")
+        return Observable.create { [weak self] observable in
+            var dataArr: [SnapShotDTO] = []
+            self?.db.collection("snapshots").getDocuments() { (querySnapshot, err) in
+                if let err = err {
+                    print("FireStore : Error getting documents: \(err)")
+                } else {
+                    let decoder =  JSONDecoder()
+                    for document in querySnapshot!.documents {
+                        do {
+                            let data = document.data()
+                            let jsonData = try JSONSerialization.data(withJSONObject:data)
+                            let markerInfo = try decoder.decode(SnapShotDTO.self, from: jsonData)
+                            dataArr.append(markerInfo)
+                            print(markerInfo)
+                        } catch let err {
+                            print("err: \(err)")
+                        }
+                        //print("FireStore: \(document.documentID) => \(document.data())")
+                    }
+                }
+            }
+            observable.onNext(dataArr)
+                
+            return Disposables.create()
+        }
+        
+    }
+}
